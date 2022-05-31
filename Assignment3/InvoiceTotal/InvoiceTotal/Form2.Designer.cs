f529567fa4ef19ee63cd63595409a0556f63d106
Adam <76805830+adam731@users.noreply.github.com>
2022-05-31 14:55:40 -0230

    Add files via upload

---
+++ Assignment3/InvoiceTotal/InvoiceTotal/Form2.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace InvoiceTotal
+{
+    partial class frmSalesTax
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtSales = new System.Windows.Forms.TextBox();
+            this.lblTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(72, 99);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click_1);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(214, 99);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtSales
+            // 
+            this.txtSales.Location = new System.Drawing.Point(189, 41);
+            this.txtSales.Name = "txtSales";
+            this.txtSales.Size = new System.Drawing.Size(100, 23);
+            this.txtSales.TabIndex = 2;
+            // 
+            // lblTxt
+            // 
+            this.lblTxt.AutoSize = true;
+            this.lblTxt.Location = new System.Drawing.Point(72, 44);
+            this.lblTxt.Name = "lblTxt";
+            this.lblTxt.Size = new System.Drawing.Size(76, 15);
+            this.lblTxt.TabIndex = 3;
+            this.lblTxt.Text = "Sales Tax pct:";
+            // 
+            // frmSalesTax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 161);
+            this.Controls.Add(this.lblTxt);
+            this.Controls.Add(this.txtSales);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Name = "frmSalesTax";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtSales;
+        private System.Windows.Forms.Label lblTxt;
+    }
+}
---
+++ Assignment3/InvoiceTotal/InvoiceTotal/Form2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InvoiceTotal
+{
+    public partial class frmSalesTax : Form
+{
+    public frmSalesTax()
+    {
+        InitializeComponent();
+    }
+                public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal number.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name,
+            decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number <= min || number >= max)
+            {
+                MessageBox.Show(name + " must be between " + min +
+                    " and " + max + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOk_Click_1(object sender, EventArgs e)
+        {
+            if (IsPresent(txtSales, "Present") && IsDecimal(txtSales, "Decimal") &&
+                IsWithinRange(txtSales, "Decimal", 0m, 100m))
+            {
+                this.Tag = txtSales.Text;
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+    }
+}
