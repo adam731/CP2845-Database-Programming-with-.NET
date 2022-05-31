9fa067a0827ba65cafc26f1d05754a42c340d020
Adam <76805830+adam731@users.noreply.github.com>
2022-05-31 14:52:08 -0230

    Add files via upload

---
+++ Assignment 3 (Part 2)/InventoryMaintenance/InvItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        private int itemNo;
+        private string description;
+        private decimal price;
+        
+        public int ItemNo
+        {
+            get { return itemNo; }  
+            set { itemNo = value; }
+        }
+
+        public string Description
+        {
+            get { return description; } 
+            set { description = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public InvItem()
+        {
+            
+        }
+
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            ItemNo = itemNo;
+            Description = description;
+            Price = price;
+        }
+
+        public string GetDisplayText()
+        {
+            return itemNo + " " + description + " " + price.ToString("c");
+        }
+    }
+}
+
+
